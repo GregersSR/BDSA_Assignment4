c9a1362850e7a50c9cd57c4f284bbe52c15c177f
Gregers Rørdam <gregers@rordam.dk>
2021-10-08 14:30:53 +0200

    Implement + test create, read, update

---
+++ Assignment4.Entities.Tests/TagRepositoryTests.cs
@@ -0,0 +1,89 @@
+using System;
+using Xunit;
+using Assignment4.Entities;
+using Assignment4.Core;
+using Assignment4;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+
+namespace Assignment4.Entities.Tests
+{
+    public class TagRepositoryTests : IDisposable
+    {
+        private readonly KanbanContext context;
+        private readonly SqliteConnection connection;
+        private readonly TagRepository repo;
+        public TagRepositoryTests() {
+            connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+            var builder = new DbContextOptionsBuilder<KanbanContext>();
+            builder.UseSqlite(connection);
+            context = new KanbanContext(builder.Options);
+            context.Database.EnsureCreated();
+            repo = new TagRepository(context);
+        }
+
+        [Fact]
+        public void Create_ReturnsCreatedAnd1_GivenTagDTO()
+        {
+            var (resp, id) = repo.Create(new TagCreateDTO { Name = "MyTag"} );
+            Assert.Equal(Response.Created, resp);
+            Assert.Equal(1, id);
+        }
+
+        [Fact]
+        public void Update_ReturnsUpdated_GivenValidId()
+        {
+            // Arrange
+            var (createResp, id) = repo.Create(new TagCreateDTO { Name = "MyTag"} );
+            Assert.Equal(Response.Created, createResp);
+            Assert.Equal(1, id);
+
+            // Act 
+            var updateResp = repo.Update(new TagUpdateDTO { Id = 1, Name = "MyTag - changed name"} );
+            Assert.Equal(Response.Updated, updateResp);
+        }
+        [Fact]
+        public void Update_ReturnsNotFound_GivenInvalidId()
+        {
+            // Act 
+            var updateResp = repo.Update(new TagUpdateDTO { Id = 42, Name = "MyTag - changed name"} );
+            Assert.Equal(Response.NotFound, updateResp);
+        }
+
+        [Fact]
+        public void Read_ReturnsTagDTO_GivenValidId()
+        {
+            // Arrange
+            var (createResp, id) = repo.Create(new TagCreateDTO { Name = "MyTag"} );
+            Assert.Equal(Response.Created, createResp);
+            Assert.Equal(1, id);
+
+            var tag = repo.Read(1);
+            Assert.Equal(new TagDTO(1, "MyTag"), tag);
+        }
+
+        [Fact]
+        public void Read_ReturnsNull_GivenInvalidId()
+        {
+            var tag = repo.Read(11);
+            Assert.Null(tag);
+        }
+
+        [Fact]
+        public void Delete_ReturnsDeleted_GivenValidId()
+        {
+            // Arrange
+            var (createResp, id) = repo.Create(new TagCreateDTO { Name = "MyTag"} );
+            Assert.Equal(Response.Created, createResp);
+            Assert.Equal(1, id);
+            
+        }
+        
+        public void Dispose()
+        {
+            context.Dispose();
+            connection.Dispose();
+        }
+    }
+}
--- Assignment4.Entities/TagRepository.cs
+++ Assignment4.Entities/TagRepository.cs
@@ -1,6 +1,52 @@
+using System.Collections.Generic;
+using Assignment4.Core;
+
 namespace Assignment4.Entities
 {
-    public class TagRepository
+    public class TagRepository : ITagRepository
     {
+        private readonly KanbanContext _context;
+        public TagRepository(KanbanContext context)
+        {
+            _context = context;
+        }
+
+        public (Response Response, int TagId) Create(TagCreateDTO tag)
+        {
+            var entity = new Tag { Name = tag.Name };
+            
+            _context.Tags.Add(entity);
+            _context.SaveChanges();
+            return (Response.Created, entity.Id);
+        }
+
+        public Response Delete(int tagId, bool force = false)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TagDTO Read(int tagId)
+        {
+            var entity = _context.Tags.Find(tagId);
+            return entity == null ? null : new TagDTO(entity.Id, entity.Name);
+        }
+
+        public IReadOnlyCollection<TagDTO> ReadAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Response Update(TagUpdateDTO tag)
+        {
+            var entity = _context.Tags.Find(tag.Id);
+            if (entity == null) {
+                return Response.NotFound;
+            }
+
+            entity.Name = tag.Name;
+            _context.SaveChanges();
+
+            return Response.Updated;
+        }
     }
 }
