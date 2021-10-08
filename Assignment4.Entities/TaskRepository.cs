cde25df47b59e72c1cfaf6f0fecf36e4853bbe70
Gyhrs <70760187+Gyhrs@users.noreply.github.com>
2021-10-08 12:44:59 +0200

    Feature/taskrepo2 (#7)
    * Made taskrepo
    
    * Fixed task enum
    
    * Prep for TaskRepo Testing

--- Assignment4.Entities/TaskRepository.cs
+++ Assignment4.Entities/TaskRepository.cs
@@ -1,6 +1,53 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Assignment4.Core;
+using System.Linq;
+
 namespace Assignment4.Entities
 {
-    public class TaskRepository
+    public class TaskRepository : Assignment4.Core.ITaskRepository
     {
+        private readonly KanbanContext context;
+
+        public TaskRepository(KanbanContext context) {
+            this.context = context;
+        }
+        
+        public IReadOnlyCollection<TaskDTO> All() {
+            var query = from c in context.Task select c;
+            return (ReadOnlyCollection<TaskDTO>) query.AsEnumerable();
+            
+        }
+        public int Create(TaskDTO task) 
+        {
+            context.Task.Add(new Task{
+                Id = task.Id,
+                Title = task.Title,
+                AssignedTo = context.User.FirstOrDefault(user => user.Id == task.AssignedToId),
+                Description = task.Description,
+                });
+            return context.SaveChanges();
+        }
+
+        public void Delete(int taskId) 
+        {
+            var query = from t in context.Task where t.Id == taskId select t;
+            context.Remove(query);
+        }
+
+        public TaskDetailsDTO FindById(int id) 
+        {
+            var query = from t in context.Task where t.Id == id select t;
+            return (TaskDetailsDTO) query;
+        }
+
+        public void Update(TaskDTO task) 
+        {
+            context.Update(task);
+        }
+
+        public void Dispose() {
+            context.Dispose();
+        }
     }
 }
