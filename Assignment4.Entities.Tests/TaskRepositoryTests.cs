40a63b7e62026875e0593e1a01502aa1d0771af8
megsikon <75022402+megsikon@users.noreply.github.com>
2021-10-13 12:36:32 +0200

    Task repo test magnus (#10)
    * Wrote xunti test
    
    * Attempted Read() testing
    
    * Read method created and tested
    
    * update not found works (#9)
    
    Co-authored-by: Magnus Emil Gervig Svensson <mesv@itu.dk>
    
    * Made Update work
    
    * Testing for some readmethods
    
    * readAllRemoved and its test
    
    Co-authored-by: Gyhrs <tugy@itu.dk>
    Co-authored-by: Gyhrs <70760187+Gyhrs@users.noreply.github.com>
    Co-authored-by: Magnus Emil Gervig Svensson <mesv@itu.dk>

--- Assignment4.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment4.Entities.Tests/TaskRepositoryTests.cs
@@ -5,22 +5,263 @@ using Assignment4.Core;
 using Assignment4;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Assignment4.Entities.Tests
 {
-    public class TaskRepositoryTests
+    public class TaskRepositoryTests : IDisposable
     {
+        private readonly SqliteConnection connection;
         private readonly KanbanContext context;
         private readonly TaskRepository repo;
         public TaskRepositoryTests() {
-            using var connection = new SqliteConnection("Filename=:memory:");
+            connection = new SqliteConnection("Filename=:memory:");
             connection.Open();
             var builder = new DbContextOptionsBuilder<KanbanContext>();
             builder.UseSqlite(connection);
-            using var _context = new KanbanContext(builder.Options);
-            _context.Database.EnsureCreated();
-            context = _context;
+            context = new KanbanContext(builder.Options);
+            context.Database.EnsureCreated();
             repo = new TaskRepository(context);
         }
+
+        [Fact]
+        public void Create_ReturnsResponseAndId_GivenTask()
+        {
+        var (resp, taskId) = repo.Create(new TaskCreateDTO{
+            Title = "Title",
+            AssignedToId = 1,
+            Description = "description",
+            Tags = new List<string> {"one", "two"}
+        });
+        Assert.Equal((Response.Created, 1),(resp, taskId));
+        }
+        [Fact]
+        public void Delete_ReturnsDeleted_GivenValidTaskId()
+        {
+        context.Task.Add(new Task {
+            Id = 1,
+        });
+        var response = repo.Delete(1);
+        Assert.Equal(Response.Deleted, response);
+        }
+        [Fact]
+        public void Delete_ReturnsNotFound_GivenInvalidId()
+        {
+            var response = repo.Delete(1);
+            Assert.Equal(Response.NotFound, response);
+        }
+
+        [Fact]
+        public void Update_ReturnsUpdated_GivenValidTaskId()
+        {
+            var task = new TaskUpdateDTO
+            {
+                Id = 1,
+                State = State.Resolved,
+            };
+            context.Task.Add(new Task {
+                Id = 1,
+            });
+            context.SaveChanges();
+            var response = repo.Update(task);
+            Assert.Equal(Response.Updated, response);
+        }
+        [Fact]
+        public void Update_ReturnsNotFound_GivenInvalidTaskId()
+        {
+            var task = new TaskUpdateDTO
+            {
+                Id = 1,
+            };
+            var response = repo.Update(task);
+
+            Assert.Equal(Response.NotFound, response);
+        }
+        [Fact]
+        public void Update_UpdatesTaskState_GivenNewState()
+        {
+            var task = new TaskUpdateDTO
+            {
+                Id = 1,
+                State = State.Resolved,
+            };
+            context.Task.Add(new Task {
+                Id = 1,
+            });
+            context.SaveChanges();
+            repo.Update(task);
+            var task1 = repo.Read(1);
+            Assert.Equal(State.Resolved, task1.State);
+        }
+        [Fact]
+        public void Read_ReturnsTaskDetailsDTO_GivenValidId()
+        {
+            var tddto = new TaskDetailsDTO (
+                1, "", "", DateTime.Today, "", new List<string>(), State.New, DateTime.Today);
+            context.Task.Add(new Task{
+                Id = 1,
+                Title = "",
+                AssignedTo = new User{Id = 1, Name = "", Email = ""},
+                Description = "",
+                State = State.New
+            });
+            context.SaveChanges();
+            var task = repo.Read(1);
+
+            Assert.Equal(1, task.Id);
+            Assert.Equal("", task.Title);
+            Assert.Equal("", task.Description);
+            Assert.Equal("", task.AssignedToName);
+            Assert.Equal(State.New, task.State);
+            Assert.Equal(DateTime.Today, task.Created, precision: TimeSpan.FromSeconds(5));
+            Assert.Equal(DateTime.Today, task.StateUpdated, precision: TimeSpan.FromSeconds(5));
+        }
+        [Fact]
+        public void ReadAll_ReturnsAListWithAllTask()
+        {
+        //Given
+        var tag = new Tag{ Id =1, Name = "name"};
+        var alltaskDTO = new List<TaskDTO>();
+        alltaskDTO.Add(new TaskDTO(1,"", "", new List<string>(){tag.Name}, State.New));
+            context.AddRange(
+            new Task{
+                Id = 1,
+                Title = "",
+                AssignedTo = new User{Id = 1, Name = "", Email = ""},
+                Description = "",
+                State = State.New,
+                Tags = new List<Tag>(){tag}
+            });
+        context.SaveChanges();
+        //When
+        var taska = repo.ReadAll().Single();
+        var taske = alltaskDTO.Single();
+        //Then
+        Assert.Equal(taske.Id,taska.Id);
+        Assert.Equal(taske.Title,taska.Title);
+        Assert.Equal(taske.AssignedToName,taska.AssignedToName);
+        Assert.Equal(taske.State,taska.State);
+        Assert.Equal(taske.Tags,taska.Tags);
+        }
+
+        [Fact]
+        public void ReadAll_ReturnsAListWithAllTask_GivenAState()
+        {
+        //Given
+        var tag = new Tag{ Id =1, Name = "name"};
+        var alltaskDTO = new List<TaskDTO>();
+        alltaskDTO.Add(new TaskDTO(1,"", "", new List<string>(){tag.Name}, State.New));
+            context.AddRange(
+            new Task{
+                Id = 1,
+                Title = "",
+                AssignedTo = new User{Id = 1, Name = "", Email = ""},
+                Description = "",
+                State = State.New,
+                Tags = new List<Tag>(){tag}
+            });
+        context.SaveChanges();
+        //When
+        var taska = repo.ReadAll().Single();
+        var taske = alltaskDTO.Single();
+        //Then
+        Assert.Equal(taske.Id,taska.Id);
+        Assert.Equal(taske.Title,taska.Title);
+        Assert.Equal(taske.AssignedToName,taska.AssignedToName);
+        Assert.Equal(taske.State,taska.State);
+        Assert.Equal(taske.Tags,taska.Tags);
+        }
+
+        [Fact]
+        public void ReadAll_ReturnsAListWithAllTask_GivenTag()
+        {
+        //Given
+        var tag = new Tag{ Id =1, Name = "name"};
+        var alltaskDTO = new List<TaskDTO>();
+        alltaskDTO.Add(new TaskDTO(1,"", "", new List<string>(){tag.Name}, State.New));
+            context.AddRange(
+            new Task{
+                Id = 1,
+                Title = "",
+                AssignedTo = new User{Id = 1, Name = "", Email = ""},
+                Description = "",
+                State = State.New,
+                Tags = new List<Tag>(){tag}
+            });
+        context.SaveChanges();
+        //When
+        var taska = repo.ReadAll().Single();
+        var taske = alltaskDTO.Single();
+        //Then
+        Assert.Equal(taske.Id,taska.Id);
+        Assert.Equal(taske.Title,taska.Title);
+        Assert.Equal(taske.AssignedToName,taska.AssignedToName);
+        Assert.Equal(taske.State,taska.State);
+        Assert.Equal(taske.Tags,taska.Tags);
+        }
+
+        [Fact]
+        public void ReadAll_ReturnsAListWithAllTask_GivenAnUser()
+        {
+        //Given
+        var tag = new Tag{ Id =1, Name = "name"};
+        var alltaskDTO = new List<TaskDTO>();
+        alltaskDTO.Add(new TaskDTO(1,"", "", new List<string>(){tag.Name}, State.New));
+            context.AddRange(
+            new Task{
+                Id = 1,
+                Title = "",
+                AssignedTo = new User{Id = 1, Name = "", Email = ""},
+                Description = "",
+                State = State.New,
+                Tags = new List<Tag>(){tag}
+            });
+        context.SaveChanges();
+        //When
+        var taska = repo.ReadAll().Single();
+        var taske = alltaskDTO.Single();
+        //Then
+        Assert.Equal(taske.Id,taska.Id);
+        Assert.Equal(taske.Title,taska.Title);
+        Assert.Equal(taske.AssignedToName,taska.AssignedToName);
+        Assert.Equal(taske.State,taska.State);
+        Assert.Equal(taske.Tags,taska.Tags);
+        }
+
+        [Fact]
+        public void ReadAll_Remove()
+        {
+             //Given
+        var tag = new Tag{ Id =1, Name = "name"};
+        var alltaskDTO = new List<TaskDTO>();
+        alltaskDTO.Add(new TaskDTO(1,"", "", new List<string>(){tag.Name}, State.Removed));
+            context.AddRange(
+            new Task{
+                Id = 1,
+                Title = "",
+                AssignedTo = new User{Id = 1, Name = "", Email = ""},
+                Description = "",
+                State = State.Removed,
+                Tags = new List<Tag>(){tag}
+            });
+        context.SaveChanges();
+        //When
+        var taska = repo.ReadAllRemoved().Single();
+        var taske = alltaskDTO.Single();
+        //Then
+        Assert.Equal(taske.Id,taska.Id);
+        Assert.Equal(taske.Title,taska.Title);
+        Assert.Equal(taske.AssignedToName,taska.AssignedToName);
+        Assert.Equal(taske.State,taska.State);
+        Assert.Equal(taske.Tags,taska.Tags);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+            connection.Dispose();
+        }
+        
     }
 }
--- Assignment4.Entities/TaskRepository.cs
+++ Assignment4.Entities/TaskRepository.cs
@@ -7,30 +7,61 @@ namespace Assignment4.Entities
 {
     public class TaskRepository : Assignment4.Core.ITaskRepository
     {
-        private readonly KanbanContext context;
+        private readonly KanbanContext _context;
 
         public TaskRepository(KanbanContext context) {
-            this.context = context;
+            this._context = context;
         }
 
         public (Response Response, int TaskId) Create(TaskCreateDTO task)
         {
-            throw new System.NotImplementedException();
+            _context.Task.Add(new Task {
+                Title = task.Title,
+                Description = task.Description,
+            });
+            return (Response.Created, _context.SaveChanges());
         }
 
         public Response Delete(int taskId)
         {
-            throw new System.NotImplementedException();
-        }
+            var entity = _context.Task.Find(taskId);
+            if (entity == null) {
+                return Response.NotFound;
+            } else {
+                _context.Task.Remove(entity);
+                _context.SaveChanges();
+                return Response.Deleted;
+            }
 
+        }
         public TaskDetailsDTO Read(int taskId)
         {
-            throw new System.NotImplementedException();
+            var task =  from t in _context.Task
+                        where t.Id == taskId
+                        select new TaskDetailsDTO(
+                            t.Id,
+                            t.Title,
+                            t.Description,
+                            System.DateTime.Today,
+                            t.AssignedTo.Name,
+                            t.Tags.Select(t => t.Name).ToList(),
+                            t.State,
+                            System.DateTime.Today
+                        );
+            return task.FirstOrDefault();
         }
 
         public IReadOnlyCollection<TaskDTO> ReadAll()
         {
-            throw new System.NotImplementedException();
+            var alltasks =  from t in _context.Task
+                            select t;
+            var alltaskslist = alltasks.ToList().AsReadOnly();
+            var alltaskslistDTO = new List<TaskDTO>();
+            foreach (var task in alltaskslist)
+            {
+                alltaskslistDTO.Add(new TaskDTO(task.Id, task.Title, task.AssignedTo.Name, task.Tags.Select(c => c.Name).ToList(), task.State));
+            }
+            return alltaskslistDTO.AsReadOnly();
         }
 
         public IReadOnlyCollection<TaskDTO> ReadAllByState(Core.State state)
@@ -50,12 +81,30 @@ namespace Assignment4.Entities
 
         public IReadOnlyCollection<TaskDTO> ReadAllRemoved()
         {
-            throw new System.NotImplementedException();
+            var alltasks =  from t in _context.Task
+                            where t.State == State.Removed
+                            select t;
+            var alltaskslist = alltasks.ToList().AsReadOnly();
+            var alltaskslistDTO = new List<TaskDTO>();
+            foreach (var task in alltaskslist)
+            {
+                alltaskslistDTO.Add(new TaskDTO(task.Id, task.Title, task.AssignedTo.Name, task.Tags.Select(c => c.Name).ToList(), task.State));
+            }
+            return alltaskslistDTO.AsReadOnly();
         }
 
         public Response Update(TaskUpdateDTO task)
         {
-            throw new System.NotImplementedException();
+            var entity = _context.Task.Find(task.Id);
+            
+            if (entity == null) {
+                return Response.NotFound;
+            }
+            entity.State = task.State;
+            
+            _context.SaveChanges();
+
+            return Response.Updated;
         }
     }
 }
